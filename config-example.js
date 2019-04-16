789276baba94e339bafd0cf64f2c0868be2d3d77
Fredd Bezerra <frederico.bezerra@bennu.tv>
2019-04-16 09:57:35 -0300

    config example

---
+++ config-example.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG  = {
+    apiKey: "your-api-key",
+    authDomain: "youApp.firebaseapp.com",
+    databaseURL: "https://YourApp.firebaseio.com",
+    projectId: "YouProjehct",
+    storageBucket: "YouApp.appspot.com",
+    messagingSenderId: "00000000000"
+};
